751fe04c34c0a29a19057de3b073632b2cedbbcb
Toby Inkster <mail@tobyinkster.co.uk>
2013-03-18 17:33:24 +0000

    factor out has "api" stuff
    --HG--
    branch : Finance-Bitcoin

---
+++ lib/Finance/Bitcoin/Role/HasAPI.pm
@@ -0,0 +1,29 @@
+package Finance::Bitcoint::Role::HasAPI;
+
+use Moo::Role;
+use Finance::Bitcoin::API;
+
+has api => (
+	is      => 'rw',
+	default => sub { "Finance::Bitcoin::API"->new },
+);
+
+around BUILDARGS => sub
+{
+	my $orig  = shift;
+	my $class = shift;
+	
+	if (scalar @_ == 1 and blessed $_[0])
+	{
+		return $class->$orig(api => @_);
+	}
+	elsif (scalar @_ == 1 and $_[0] =~ /^http/)
+	{
+		my $api = "Finance::Bitcoin::API"->new(endpoint => "$_[0]");
+		return $class->$orig(api => $api);
+	}
+	
+	return $class->$orig(@_);
+};
+
+1;
